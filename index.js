ea522a79d0e3272f6bb85427579b73ce13ef2cb0
ShaonlyB <shaonly.bhattacharya@bitcanny.com>
2024-11-13 07:20:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// console.log("hello");
+const redux= require ("redux")
+const createStore= redux.createStore
+const Buy_Cake="Buy_Cake"
+
+function buyCake(){
+    return {
+        type: Buy_Cake,
+        info: "first redux action"
+    }
+}
+
+//(prevState, action)=> newState
+
+const initialState={
+    numOfCakes: 10
+}
+
+const reducer=(state=initialState, action)=>{
+    switch (action.type) {
+        case Buy_Cake:
+            return{
+                ...state,  //copy the initilaState object
+                numOfCakes: state.numOfCakes-1
+            }
+    
+        default:
+           return state
+    }
+}
